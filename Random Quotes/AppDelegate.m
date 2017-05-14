6dcb84a40302db00c2c822ed75a47a5b32df52eb
ccoeder <caanyildirims@gmail.com>
2017-05-14 18:24:41 +0300

    activity indicator added.

--- Random Quotes/AppDelegate.m
+++ Random Quotes/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <RestKit/RestKit.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [AFRKNetworkActivityIndicatorManager sharedManager].enabled = YES;
+    
     return YES;
 }
 
